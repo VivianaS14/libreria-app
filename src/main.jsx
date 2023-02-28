02a508c1467bf3b7dce0cbb10560e8c72e09bf57
VivianaS14 <vasorianp@gmail.com>
2023-02-27 21:18:22 -0500

    fix: context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import Context from "./context/context";
+import Context from "./context/Context";
 import App from "./routes/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
