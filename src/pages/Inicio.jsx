75ca994e29fb6202c8289886335e2d2ac4d669d2
andres0818 <c.andres.perez.h@gmail.com>
2023-02-28 20:16:07 -0500

    add services and most read

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Login from "../components/Login/Login";
+import MostRead from "../components/MostRead/MostRead";
+import Service from "../components/ServicesUser/Service";
 
 const Inicio = () => {
   return (
     <div>
       <div className="Inicio">
         <h1>Soy el inicio</h1>
-        <Login/>
+        <Login />
+        <MostRead />
+        <Service />
       </div>
     </div>
   );
