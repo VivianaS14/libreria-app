1e0b667a1296d6073af036909c42d5a03957e795
andres0818 <c.andres.perez.h@gmail.com>
2023-03-01 23:01:09 -0500

    user card rendering

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,20 +1,28 @@
-import React from "react";
-import user from "../Login/img/user.svg";
+import React, { useContext } from "react";
+import { AppContext } from "../../context/Context";
+import userDefault from "../Login/img/user.svg";
 
-import "./User.scss";
 
 const User = () => {
+  const { picture, user } = useContext(AppContext);
+  
+const {fullName, email, }= user
+
   return (
     <div className="d-flex d-flex justify-content-around  align-items-center  border p-5 m-5 position-relative">
       <button className="btn btn-danger position-absolute top-0 end-0 m-5">
         Cerrar sesion
       </button>
 
-      <img style={{ width: "200px" }} src={user} alt="" />
+      <img
+        style={{ width: "200px" }}
+        src={picture !== null ? picture : userDefault}
+        alt=""
+      />
       <div className="col-5">
-        <h1> name</h1>
-        <p>Email</p>
-        <p>Libros cambiados</p>
+        <h1> {fullName ? fullName : "Nombre de usuario"}</h1>
+        <p>{email ? email : "emial"}</p>
+        <p>Libros cambiados: 0 </p>
       </div>
     </div>
   );
