0c720abdf90e5e8d17a3e7dd0749b3b8a543708a
jandres-vega <jandresvega@itsa.edu.co>
2023-03-02 09:29:47 -0500

    Feact: modified component of page intercambiar

--- src/pages/Intercambiar.jsx
+++ src/pages/Intercambiar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import BooksMain from "../components/AllBooks/BooksMain.jsx";
+import {theme} from "../theme/theme.js";
+import {ThemeProvider} from "@mui/material/styles";
 
 const Intercambiar = () => {
   return (
     <div>
       <div className="Intercambiar">
-        <h3>Intercambio</h3>
+          <ThemeProvider theme={theme}>
+              <BooksMain />
+          </ThemeProvider>
       </div>
     </div>
   );
