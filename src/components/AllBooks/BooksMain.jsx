f685341235e3128c0ef243fbccfd501862e6a3b8
VivianaS14 <vasorianp@gmail.com>
2023-03-03 22:08:09 -0500

    feat: detail intercambiar page

--- src/components/AllBooks/BooksMain.jsx
+++ src/components/AllBooks/BooksMain.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {data} from './utils/allBooks';
-import {Box, Container, Grid, Typography} from "@mui/material";
+import React from "react";
+import { data } from "./utils/allBooks";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import CardBook from "./CardBook/CardBook.jsx";
 import Paginated from "./Paginado/Paginated.jsx";
 import FilterByGenres from "./Filters/FilterByGenres";
@@ -10,70 +10,92 @@ import OrderBooks from "./Filters/OrderBooks.jsx";
 import MenuIFilter from "./MenuDrawer/MenuIFilter.jsx";
 
 const BooksMain = () => {
+  const [currentPag, setCurrentPag] = React.useState(1);
+  const [booksByPag] = React.useState(6);
+  const indexByBooks = booksByPag * currentPag;
+  const indexPrimerBook = indexByBooks - booksByPag;
+  const currentBooks = data.slice(indexPrimerBook, indexByBooks);
+  const paginado = (event, paginado) => {
+    setCurrentPag(paginado);
+  };
+  return (
+    <React.Fragment>
+      <h2>Intercambia y Disfruta</h2>
+      <Box
+        sx={{
+          display: "flex",
+          // justifyContent: 'flex-end',
+          justifyContent: {
+            xs: "space-between",
+            lg: "flex-end",
+            md: "flex-end",
+          },
+          fontFamily: "Radley, serif",
+          mr: 4,
+          mt: 2,
+          mb: 2,
+        }}
+      >
+        <MenuIFilter />
+        <OrderBooks />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          fontFamily: "Radley, serif",
+        }}
+      >
+        <Box
+          sx={{
+            fontFamily: "Radley, serif",
+            mb: 2,
+            width: 250,
+            height: 500,
+            ml: 4,
+            display: { xs: "none", md: "block" },
+          }}
+        >
+          <Typography variant="h5" sx={{ mb: 2, fontFamily: "Radley, serif" }}>
+            Filtrar por:{" "}
+          </Typography>
+          <FilterByGenres />
+          <FilterByLenguajes />
+          <FilterByAuthors />
+        </Box>
 
-    const [currentPag, setCurrentPag] = React.useState(1);
-    const [booksByPag] = React.useState(6);
-    const indexByBooks = booksByPag * currentPag;
-    const indexPrimerBook = indexByBooks - booksByPag;
-    const currentBooks = data.slice(indexPrimerBook, indexByBooks);
-    const paginado = (event, paginado) => {
-        setCurrentPag(paginado)
-    }
-    return (
-        <React.Fragment>
-            <Box sx={{
-                    display: 'flex',
-                    // justifyContent: 'flex-end',
-                    justifyContent:{xs:'space-between' , lg: 'flex-end', md: 'flex-end'},
-                    mr:4, mt:2, mb:2}}
-            >
-                <MenuIFilter />
-                <OrderBooks />
-            </Box>
-            <Box sx={{display:'flex', flexDirection: 'row'}}>
-                <Box
-                    sx={{
-                        mb: 2,
-                        width: 250,
-                        height: 500,
-                        ml: 4,
-                        display:{xs: 'none', md:'block'}
-                    }}
-                >
-                    <Typography variant="h5" sx={{mb:2}}>Filtrar por: </Typography>
-                    <FilterByGenres/>
-                    <FilterByLenguajes />
-                    <FilterByAuthors />
-                </Box>
-
-                <Container maxWidth="lg">
-                    <Grid maxWidth="lg" container columns={{xs: 2, sm:2, md:8, lg: 12}}>
-                        {
-                            currentBooks.map(book => (
-                                <Grid item sx={{mb:2, pl:1}} xs={2} sm={4} md={4} key={book.id}>
-                                    <CardBook
-                                        nameBook={book.name_book}
-                                        image={book.image}
-                                        author={book.author}
-                                        status={book.status}
-                                        language={book.Language}
-                                    />
-                                </Grid>
-                            ))
-                        }
-                    </Grid>
-                </Container>
-            </Box>
-            <Box sx={{display: 'flex', justifyContent: 'center',mb: 4}}>
-                <Paginated
-                    booksByPage={booksByPag}
-                    books={data}
-                    paginado={paginado}
-
+        <Container maxWidth="lg">
+          <Grid
+            maxWidth="lg"
+            container
+            columns={{ xs: 2, sm: 2, md: 8, lg: 12 }}
+          >
+            {currentBooks.map((book) => (
+              <Grid
+                item
+                sx={{ mb: 2, pl: 1 }}
+                xs={2}
+                sm={4}
+                md={4}
+                key={book.id}
+              >
+                <CardBook
+                  nameBook={book.name_book}
+                  image={book.image}
+                  author={book.author}
+                  status={book.status}
+                  language={book.Language}
                 />
-            </Box>
-        </React.Fragment>
-    );
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center", mb: 4 }}>
+        <Paginated booksByPage={booksByPag} books={data} paginado={paginado} />
+      </Box>
+    </React.Fragment>
+  );
 };
 
 export default BooksMain;
--- src/components/AllBooks/MenuDrawer/MenuIFilter.jsx
+++ src/components/AllBooks/MenuDrawer/MenuIFilter.jsx
@@ -1,59 +1,80 @@
-import React from 'react';
-import {Box, Button, Divider, Drawer, IconButton, List, ListItem, styled, useTheme} from "@mui/material";
+import React from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  styled,
+  useTheme,
+} from "@mui/material";
 import FilterByLenguajes from "../Filters/FilterByLenguajes.jsx";
 import FilterByGenres from "../Filters/FilterByGenres.jsx";
 import FilterByAuthors from "../Filters/FilterByAuthors.jsx";
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 const MenuIFilter = () => {
-    const themeUI = useTheme();
-    const DrawerHeader = styled('div')(({ theme }) => ({
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    }));
-    const [open, setOpen] = React.useState(false);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const themeUI = useTheme();
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  }));
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <React.Fragment>
-            <Button
-                color="secondary"
-                onClick={handleDrawerOpen}
-                variant="text"
-                sx={{ mr: 2, ...(open && { display: 'none' }), display: {lg: 'none', xs: 'flex', md: 'none'} }}
-            >Filtrar por:
-            </Button>
-            <Drawer
-                variant="persistent"
-                anchor="left"
-                open={open}
+  return (
+    <React.Fragment>
+      <Button
+        color="secondary"
+        onClick={handleDrawerOpen}
+        variant="text"
+        sx={{
+          mr: 2,
+          ...(open && { display: "none" }),
+          display: { lg: "none", xs: "flex", md: "none" },
+        }}
+      >
+        Filtrar por:
+      </Button>
+      <Drawer variant="persistent" anchor="left" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {themeUI.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                fontFamily: "Radley, serif",
+              }}
             >
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {themeUI.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <List>
-                    <ListItem>
-                        <Box sx={{display: 'flex', flexDirection: 'column'}}>
-                            <FilterByLenguajes />
-                            <FilterByGenres />
-                            <FilterByAuthors />
-                        </Box>
-                    </ListItem>
-                </List>
-            </Drawer>
-        </React.Fragment>
-    );
+              <FilterByLenguajes />
+              <FilterByGenres />
+              <FilterByAuthors />
+            </Box>
+          </ListItem>
+        </List>
+      </Drawer>
+    </React.Fragment>
+  );
 };
 
 export default MenuIFilter;
