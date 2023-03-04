91ddf70c7d710ff96eb33afa2d3ba9dd3bbe3d06
jandres-vega <jandresvega@itsa.edu.co>
2023-03-04 14:10:05 -0500

    Feact: modified card agregate icons

--- src/components/AllBooks/CardBook/CardBook.jsx
+++ src/components/AllBooks/CardBook/CardBook.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import {Box, Card, CardContent, CardMedia, Typography} from "@mui/material";
+import {Box, Button, Card, CardContent, CardMedia, IconButton, Typography} from "@mui/material";
+import {FavoriteBorder, Book} from '@mui/icons-material/';
+
 const CardBook = ({nameBook, image, status, author, language}) => {
+
+    const [stateHeart, setStateHeart] = React.useState(false);
+
+    const handleHeart = () => {
+        setStateHeart(!stateHeart)
+    }
+
     return (
         <Card sx={{ display: 'flex', minHeight:250 }}>
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
                 <CardContent sx={{ flex: '1 0 auto', width:150 }}>
                     <Box sx={{height:100}}>
+                        <IconButton onClick={handleHeart}>
+                            {stateHeart ? <FavoriteBorder color="secondary"/> : <FavoriteBorder/>}
+                        </IconButton>
                         <Typography variant="h5" sx={{fontFamily: 'bold'}}>{nameBook}</Typography>
                     </Box>
                     <Typography >Author: {author}</Typography>
@@ -17,6 +29,10 @@ const CardBook = ({nameBook, image, status, author, language}) => {
 
                     }
                     <Typography >Idioma: {language}</Typography>
+                    <Button variant="contained"  endIcon={<Book />}>
+                        Intercambia
+                    </Button>
+
                 </CardContent>
                 <CardMedia
                     component="img"
