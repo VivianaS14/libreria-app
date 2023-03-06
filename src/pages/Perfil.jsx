92fb43be6042424b33a1c731fca02b57313796c9
jandres-vega <jandresvega@itsa.edu.co>
2023-03-06 18:52:35 -0500

    Feact: modified pages Perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -3,22 +3,26 @@ import User from "../components/User/User";
 
 import BtnRegisterBook from "../components/RegisterBook/BtnRegisterBook.jsx";
 import {Grid} from "@mui/material";
+import {theme} from "../theme/theme.js";
+import {ThemeProvider} from "@mui/material/styles";
 
 const Perfil = () => {
   return (
       <div className="Perfil">
           <User/>
-          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
-              <Grid item xs={2} sm={4} md={4}>
-                  <BtnRegisterBook />
+          <ThemeProvider theme={theme}>
+              <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+                  <Grid item xs={2} sm={4} md={4}>
+                      <BtnRegisterBook />
+                  </Grid>
+                  <Grid item xs={2} sm={4} md={4}>
+                      <BtnRegisterBook />
+                  </Grid>
+                  <Grid item xs={2} sm={4} md={4}>
+                      <BtnRegisterBook />
+                  </Grid>
               </Grid>
-              <Grid item xs={2} sm={4} md={4}>
-                  <BtnRegisterBook />
-              </Grid>
-              <Grid item xs={2} sm={4} md={4}>
-                  <BtnRegisterBook />
-              </Grid>
-          </Grid>
+          </ThemeProvider>
       </div>
   );
 };
