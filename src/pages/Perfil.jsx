5ef5a89a17e423720ff87708b5bafb24af3ff6d2
jandres-vega <jandresvega@itsa.edu.co>
2023-03-06 12:40:45 -0500

    modified form

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import User from "../components/User/User";
+
 import BtnRegisterBook from "../components/RegisterBook/BtnRegisterBook.jsx";
 import {Grid} from "@mui/material";
 
 const Perfil = () => {
   return (
       <div className="Perfil">
-        <User/>
+          <User/>
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
               <Grid item xs={2} sm={4} md={4}>
                   <BtnRegisterBook />
