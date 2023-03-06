9abf3d24c00fb660bf4f40b58faa8ba3ddec01d4
jandres-vega <jandresvega@itsa.edu.co>
2023-03-06 13:26:00 -0500

    Feact: modified pages styles

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -8,7 +8,7 @@ const Perfil = () => {
   return (
       <div className="Perfil">
           <User/>
-          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
               <Grid item xs={2} sm={4} md={4}>
                   <BtnRegisterBook />
               </Grid>
