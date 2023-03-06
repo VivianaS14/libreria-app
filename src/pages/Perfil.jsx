95989c5e599f553b14bf270146435966b34f440f
jandres-vega <jandresvega@itsa.edu.co>
2023-03-06 12:11:15 -0500

    Feact: add component registerBook to page profile

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import User from "../components/User/User";
+//import FormRegisterBook from "../components/RegisterBook/FormRegisterBook.jsx";
+import BtnRegisterBook from "../components/RegisterBook/BtnRegisterBook.jsx";
+import {Grid} from "@mui/material";
 
 const Perfil = () => {
   return (
     <div>
       <div className="Perfil">
         <User/>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+              <Grid item xs={2} sm={4} md={4}>
+                  <BtnRegisterBook />
+              </Grid>
+              <Grid item xs={2} sm={4} md={4}>
+                  <BtnRegisterBook />
+              </Grid>
+              <Grid item xs={2} sm={4} md={4}>
+                  <BtnRegisterBook />
+              </Grid>
+          </Grid>
+
       </div>
     </div>
   );
