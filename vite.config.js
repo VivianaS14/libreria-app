1fcfe0e4d8d42c3f07c83e852740df4cc88360d0
andres0818 <c.andres.perez.h@gmail.com>
2023-03-07 12:02:20 -0500

    config store

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000, // Tamaño límite del fragmento en kB
+  },
+  rollupOptions: {
+    treeshake: {
+      experimentalChunkRequestStrategy: {
+        chunkSizeWarningLimit: 1500, // Tamaño límite del fragmento en kB
+      },
+    },
+  },
+});
