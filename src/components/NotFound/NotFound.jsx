d2459fbd833d89efdba5f947f2604a3b51f0542c
andres0818 <c.andres.perez.h@gmail.com>
2023-03-07 21:48:08 -0500

    change animation

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import cryingBook from './cryingBook.png';
 import './NotFound.scss';
 
 const NotFound = () => {
+  const [bookAnimation, setBookAnimation] = useState(0);
+  const [messageAnimation, setMessageAnimation] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBookAnimation((prev) => prev === 2 ? 0 : prev + 1);
+    }, 2000);
+
+    setTimeout(() => {
+      setMessageAnimation(true);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="not-found-container">
-      <img className="crying-book" src={cryingBook} alt="Book crying" />
-      <h2 className="not-found-message">¡Ups! La ruta que buscas no se ha encontrado.</h2>
+      <img className={`crying-book book-${bookAnimation}`} src={cryingBook} alt="Book crying" />
+      <h2 className={`not-found-message ${messageAnimation ? 'show-message' : ''}`}>¡Ups! La ruta que buscas no se ha encontrado.</h2>
     </div>
   );
 };
