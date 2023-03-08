8e1455cc7512d43668d0da12c583bb2bb67d993c
jandres-vega <jandresvega@itsa.edu.co>
2023-03-07 19:10:31 -0500

    Feact: modified component registerBook

--- src/components/RegisterBook/BtnRegisterBook.jsx
+++ src/components/RegisterBook/BtnRegisterBook.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@mui/material";
 import FormRegisterBook from "./FormRegisterBook.jsx";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-const BtnRegisterBook = () => {
+const BtnRegisterBook = ({formRegister, textTitle}) => {
     return (
         <Accordion sx={{mb: 2, bgcolor: "#9b4819"}}>
             <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
             >
-                <Typography>Registrar libros</Typography>
+                <Typography>{textTitle}</Typography>
             </AccordionSummary>
             <AccordionDetails sx={{bgcolor: "#f4f1e8"}}>
-                <FormRegisterBook />
+                {formRegister}
             </AccordionDetails>
         </Accordion >
     );
