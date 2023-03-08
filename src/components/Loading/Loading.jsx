6814592feb0e7afad1abc8129393c4f99afec9fd
andres0818 <c.andres.perez.h@gmail.com>
2023-03-07 21:28:28 -0500

    create component loadin

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <>
+      {/* <div class="loading">
+  <div class="book">
+    <div class="book-cover"></div>
+    <div class="book-pages">
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+    </div>
+  </div>
+</div> */}
+
+{/* <div className="loading">
+<div className="loaddin__page"></div>
+</div> */}
+
+<div class="loading">
+  <div class="book">
+    <div class="book-cover"></div>
+    <div class="book-pages">
+      <div class="page page-1"></div>
+      <div class="page page-2"></div>
+      <div class="page page-3"></div>
+      <div class="page page-4"></div>
+      <div class="page page-5"></div>
+      <div class="page page-6"></div>
+      <div class="page page-7"></div>
+      <div class="page page-8"></div>
+    </div>
+  </div>
+</div>
+
+    </>
+  );
+};
+
+export default Loading;
--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -3,11 +3,12 @@ import Events from "../../components/EventsUser/Events";
 import MostRead from "../../components/MostRead/MostRead";
 import Service from "../../components/ServicesUser/Service";
 import InicioContainer from "../../containers/Inicio/InicioContainer";
+import Loading from "../../components/Loading/Loading";
 import "./Inicio.scss";
-
 const Inicio = () => {
   return (
     <div className="Inicio">
+      <Loading />
       <InicioContainer />
       <Events />
       <MostRead />
