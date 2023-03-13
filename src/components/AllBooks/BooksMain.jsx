eb7cac0d8f4d997cbf8230ca1492b734a4aadc88
jandres-vega <jandresvega@itsa.edu.co>
2023-03-13 18:16:59 -0500

    Feact modified component BookksMain and util

--- src/components/AllBooks/BooksMain.jsx
+++ src/components/AllBooks/BooksMain.jsx
@@ -24,6 +24,7 @@ const BooksMain = () => {
   const paginado = (event, paginado) => {
     setCurrentPag(paginado);
   };
+
   React.useEffect(() => {
     dispach(getBooks());
   }, []);
--- src/components/AllBooks/utils/orderBooks.js
+++ src/components/AllBooks/utils/orderBooks.js
@@ -6,21 +6,11 @@ export const dataOrder = [
     },
     {
         id: 2,
-        orderBy:'Puntuacion max - min',
-        value: 'max-min'
-    },
-    {
-        id: 3,
-        orderBy:'Puntuacion min - max',
-        value: 'min-max'
-    },
-    {
-        id: 4,
         orderBy:'Nombre A - Z',
         value: 'A-Z'
     },
     {
-        id: 5,
+        id: 3,
         orderBy:'Nombre Z - A',
         value: 'Z-A'
     },
