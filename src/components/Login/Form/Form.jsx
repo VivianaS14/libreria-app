fd131dc5a9f7af587ced24773b23ed7201737b67
andres0818 <c.andres.perez.h@gmail.com>
2023-03-16 14:24:01 -0500

    correcion google

--- src/components/Login/Form/Form.jsx
+++ src/components/Login/Form/Form.jsx
@@ -126,7 +126,7 @@ const Form = () => {
         </p>
         <button onClick={loginGoogle} className="Login__buttonGoogle">
           Iniciar con
-          <span className="Login__google">Google</span>
+          <span className="Login__google"> Google</span>
         </button>
       </form>
     </>
