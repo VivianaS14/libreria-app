5e85460fc0271328b5d6efdbb61676f164383eef
VivianaS14 <vasorianp@gmail.com>
2023-03-21 11:24:37 -0500

    fix: navbar mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,89 +20,92 @@ const Navbar = () => {
     setAlert({ type: "info", message: "Sesión cerrada" });
   };
 
+  const onCollapse = () => {
+    document.getElementById("navbarNav").classList.toggle("collapse");
+  };
+
   return (
-    <div className="Navbar">
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <div className="navbar-brand Navbar__logo">
-            <Link to="/">
-              <img src={Logo} alt="Logo App" width="60" />
-            </Link>
-          </div>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <Menu sx={{ fontSize: 40, color: "#9b4819" }} />
-          </button>
-          <div
-            className="collapse navbar-collapse justify-content-end"
-            id="navbarNav"
-          >
-            <ul className="navbar-nav Navbar__items">
-              <li className="nav-item">
-                <Link
-                  to="/"
-                  className="nav-link item__link"
-                  aria-current="page"
-                >
-                  Inicio
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/intercambiar"
-                  className="nav-link item__link"
-                  aria-current="page"
-                >
-                  Intercambiar
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/servicios"
-                  className="nav-link item__link"
-                  aria-current="page"
-                >
-                  Servicios
-                </Link>
-              </li>
-              {statusLogin ? (
-                <>
-                  <li className="nav-item">
-                    <Link
-                      to="/perfil"
-                      className="nav-link item__link"
-                      aria-current="page"
-                    >
-                      Perfil
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link
-                      to="/"
-                      className="nav-link item__link"
-                      aria-current="page"
-                    >
-                      <Logout sx={{ fontSize: 30 }} onClick={logOut} />
-                    </Link>
-                  </li>
-                </>
-              ) : (
+    <>
+      <div className="Navbar">
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <div className="navbar-brand Navbar__logo">
+              <Link to="/">
+                <img src={Logo} alt="Logo App" width="60" />
+              </Link>
+            </div>
+            <button
+              id="collapse-btn"
+              className="navbar-toggler"
+              type="button"
+              onClick={onCollapse}
+            >
+              <Menu sx={{ fontSize: 40, color: "#9b4819" }} />
+            </button>
+            <div
+              id="navbarNav"
+              className="collapse navbar-collapse justify-content-end"
+            >
+              <ul className="navbar-nav Navbar__items">
                 <li className="nav-item">
-                  <Login />
+                  <Link
+                    to="/"
+                    className="nav-link item__link"
+                    aria-current="page"
+                  >
+                    Inicio
+                  </Link>
                 </li>
-              )}
-            </ul>
+                <li className="nav-item">
+                  <Link
+                    to="/intercambiar"
+                    className="nav-link item__link"
+                    aria-current="page"
+                  >
+                    Intercambiar
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/servicios"
+                    className="nav-link item__link"
+                    aria-current="page"
+                  >
+                    Servicios
+                  </Link>
+                </li>
+                {statusLogin ? (
+                  <>
+                    <li className="nav-item">
+                      <Link
+                        to="/perfil"
+                        className="nav-link item__link"
+                        aria-current="page"
+                      >
+                        Perfil
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link
+                        to="/"
+                        className="nav-link item__link"
+                        aria-current="page"
+                      >
+                        <Logout sx={{ fontSize: 30 }} onClick={logOut} />
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <li className="nav-item">
+                    <Login />
+                  </li>
+                )}
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
